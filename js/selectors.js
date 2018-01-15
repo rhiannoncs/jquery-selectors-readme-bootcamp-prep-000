2ef06075ec30ae545b4e3b0a91ab2519918ab58c
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-15 02:50:26 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,19 +1,19 @@
 function paragraphSelector() {
-  $('p');
+  return $('p');
 }
 
 function lastImageSelector() {
-  $('img:last');
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector () {
-  $('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {
-  $('#pic-list li:first-child');
+  return $('#pic-list li:first-child');
 }
